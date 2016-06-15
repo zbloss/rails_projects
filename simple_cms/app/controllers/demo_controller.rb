933b69b274ce122722dd654cfac98369d49cb7dc
zbloss <zbloss@emich.edu>
2016-06-15 14:26:05 -0400

    committed right after editing database.yml changes

--- simple_cms/app/controllers/demo_controller.rb
+++ simple_cms/app/controllers/demo_controller.rb
@@ -3,4 +3,20 @@ class DemoController < ApplicationController
 
   def index
   end
+
+  def hello
+      #render('demo/index')
+     @array = [1,2,3,4,5]
+     @id = params['id'].to_i
+     @page = params[:page].to_i
+  end
+
+  def other_hello
+      redirect_to(:controller => 'demo', :action => 'index')
+  end
+
+  def lynda
+      redirect_to("http://lynda.com")
+  end
+
 end
--- simple_cms/config/routes.rb
+++ simple_cms/config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'demo/index'
+
+    root 'demo#index'
+
+    #get 'demo/index'
+    match ':controller(/:action(/:id))', :via => :get
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
